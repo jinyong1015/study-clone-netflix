0b77f0c7fe6140a63a9447d7971bf4d4e13b251d
jinyong1015 <jjyim1015@gmail.com>
2023-09-05 22:04:32 +0900

    feat: Row 페이지 구현

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import './App.css';
+import requests from './api/requests';
 import Banner from './components/Banner';
 import Nav from './components/Nav';
+import Row from './components/Row';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Nav />
       <Banner />
+      <Row 
+        title ="NETFLIX ORIGINALS"
+        id="NO"
+        fetchURl={requests.fetchNetflixOriginals} 
+        isLargeRow 
+      />
+
+      <Row title ="Trending Now" id="TN" fetchURl={requests.fetchTrending} />
+      <Row title ="Top Rated" id="TR" fetchURl={requests.fetchTopRated} />
+      <Row title ="Action Movies" id="AM" fetchURl={requests.fetchActionMovies} />
+      <Row title ="Comedy Movies" id="CM" fetchURl={requests.fetchComedyMovies} />
     </div>
   );
 }
 
 export default App;
+
