39babf0d69045898381cc1dab35f4f089ef19b34
jinyong1015 <jjyim1015@gmail.com>
2023-11-23 19:19:31 +0900

    feat: useParams와 useref 사용해 영화 상세페이지 및 클릭시 모달 닫게 만드는 custom hook 생성

--- src/Pages/SearchPage/index.js
+++ src/Pages/SearchPage/index.js
@@ -1,10 +1,11 @@
 import axios from '../../api/axios';
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import "./SearchPage.css";
 import { useDebounce } from '../../hooks/useDebounce';
 
 export default function SearchPage() {
+  const navigate = useNavigate();
   const [searchResults, setSearchResults] = useState([]);
   
   const useQuery = () => {
@@ -42,7 +43,7 @@ export default function SearchPage() {
             "https://image.tmdb.org/t/p/w500" + movie.backdrop_path
             return (
               <div className='movie' key={movie.id}>
-                <div
+                <div onClick={() => navigate(`/${movie.id}`)}
                 className='movie__column-poster'
                 >
                   <img
